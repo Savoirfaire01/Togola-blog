b27ce1ad80ccf68200ae87f8cd849bf7ea2b9acc
Togola <togolafousseni19@gmail.com>
2023-05-13 18:30:00 +0000

    First commit

---
+++ src/posts/views.py
@@ -0,0 +1,67 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+
+from posts.models import BlogPost
+
+from django.urls import reverse_lazy
+
+"""
+
+class HomePageView(TemplateView):
+    template_name = 'posts/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['links'] = [
+            ('home', reverse_lazy('home')),
+            ('create', reverse_lazy('create')),
+            ('post', reverse_lazy('post')),
+            ('edit', reverse_lazy('edit')),
+            ('delete', reverse_lazy('delete')),
+        ],
+        return context 
+   """ 
+
+class BlogHome(ListView):
+    model = BlogPost
+    context_object_name = "posts"
+    
+    def get_queryset(self):
+
+       queryset = super().get_queryset()
+       if self.request.user.is_authenticated:
+           return queryset
+       return queryset.filter(published=True)
+
+
+class BlogPostCreate(CreateView):
+    model = BlogPost
+    template_name = "posts/blogpost_create.html"
+    fields = ['title', 'content',  ]
+
+#@method_decorator(login_required, name='dispatch')
+class BlogPostUpdate(UpdateView):
+    model = BlogPost
+    template_name = "posts/blogpost_edit.html"
+    fields = ['title', 'content', 'published', ]
+
+#@method_decorator(login_required, name='dispatch')
+class BlogPostDetail(DetailView):
+    model = BlogPost
+    context_object_name = "post"
+
+
+#@method_decorator(login_required, name='dispatch')
+class BlogPostDelete(DeleteView):
+    model = BlogPost
+    context_object_name = "post"
+    success_url = reverse_lazy("posts:home")
+
+
+
+
